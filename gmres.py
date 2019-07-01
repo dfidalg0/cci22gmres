2d64a946461afb5efaf6bc494b4c9bd6c538205e
Diego <diego-fidalgo@hotmail.com>
2019-07-01 17:09:38 -0300

    Implementation without any optimization

--- gmres.py
+++ gmres.py
@@ -1,7 +1,8 @@
+# Importando funções do numpy e de algebra linear
 from utils import *
 
 def GMRES (A,b,tolerance = 1e-6):
-    _testForErrors(A,b)
+    _testForErrors(A,b) # Verificação de erros
 
     n = b.size
 
@@ -9,11 +10,43 @@ def GMRES (A,b,tolerance = 1e-6):
     H = zeros([n+1,n])
     Q = zeros([n,n])
 
-    rotations = [None]*n
-    rotations[0] = (0,1) # Rotação de 0 rad
+    h = norm(v)
+
+    u = array([h] + [0]*n) # u = ||b||*e1
+
+    rotations = [None]*n # Vetor para armazenar as rotações de Given acumuladas
 
     for j in range(n):
-        
+        Q[:,j] = v/h
+        v = A.dot(Q[:,j])
+
+        H[0,j] = Q[:,0].dot(v)
+        v -= H[0,j]*Q[:,0]
+
+        for i in range(j):
+            H[i+1,j] = Q[:,i+1].dot(v)
+            v -= H[i+1,j]*Q[:,i+1]
+            # Aplicação das rotações de Givens acumuladas
+            H[i:i+2,j] = applyGivensRotation(rotations[i],H[i:i+2,j])
+
+        h = norm(v)
+        H[j+1,j] = h
+
+        rot = rotations[j] = getGivensRotation(H[j:j+2,j])
+
+        H[j:j+2,j] = applyGivensRotation(rot,H[j:j+2,j])
+        u[j:j+2] = applyGivensRotation(rot,u[j:j+2])
+
+        z = triangularSolve(H[:j+1,:j+1],u[:j+1])
+
+        x = Q[:,:j+1].dot(z)
+
+        err = norm(A.dot(x) - b)/norm(b)
+
+        if  err < tolerance:
+            return x
+
+    raise LinAlgError ('Singular Matrix') # Matriz muito mal condicionada
 
 def _testForErrors (A,b):
     err = (
---
+++ tests.py
@@ -0,0 +1,48 @@
+# Testes de funcionamento do GMRES
+from gmres import *
+from numpy.random import rand, randint
+from numpy.linalg import solve
+from timeit import default_timer
+
+# Teste 1 - Sistema denso
+
+n = 100
+
+A = rand(n,n)*100
+
+b = rand(n)*100
+
+t0 = default_timer()
+try:
+    x = GMRES(A,b)
+    t1 = default_timer()
+except:
+    print('Something went wrong')
+else:
+    print('Successfully executed')
+    print('Time elapsed for dense system:',t1-t0)
+
+# Teste 2 - Sistema esparso
+
+for i in range(100*50):
+    A[randint(100)][randint(100)] = 0
+
+t0 = default_timer()
+try:
+    x = GMRES(A,b)
+    t1 = default_timer()
+except:
+    print('Something went wrong')
+else:
+    print('Successfully executed')
+    print('Time elapsed for sparse system:',t1-t0)
+
+# Teste 3 - Sistema singular
+
+A[1,:] = A[0,:]
+try:
+    GMRES(A,b)
+except LinAlgError:
+    print('Handling singular matrixes properly')
+else:
+    print('Singular matrixes not being properly handled')
